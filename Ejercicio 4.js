929cb45327e8fcd7a3ca6782fbd2a6ac8967bebb
revirxx <alexisaguilera555@gmail.com>
2025-01-20 19:27:21 -0300

    ejercicio prompt

---
+++ Ejercicio 4.js
@@ -0,0 +1,10 @@
+let a = 10; //Se setea el valor de a
+console.log(a) //Se imprime a
+debugger 
+alert(a)
+
+/*
+hisadsannijcoasmcnoicas
+sadaksjdklajdkas
+klajldslkjahdlka
+*/
---
+++ Prompt/prompt.js
@@ -0,0 +1,1 @@
+alert(prompt("Hola, ¿cómo estás?"))
