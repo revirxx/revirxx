f2288d3703febbdbf239638e53567dc25e95a11b
revirxx <alexisaguilera555@gmail.com>
2025-01-21 19:16:27 -0300

    variables

---
+++ Variables/variables.js
@@ -0,0 +1,19 @@
+// VARIABLES
+// MALAS PRACTICAS
+var persona1 = {
+    nombres: "Alexis",
+    apellidos: "Aguilera",
+    años: "29"
+};
+
+// BUENAS PRACTICAS
+let nombres = "Jacinta Eulogia Barrios";
+let costo_presupuesto = 89
+
+// CONSTANTES
+const nocambia = false;
+
+console.log(typeof(persona1))
+console.log(typeof(nombres))
+console.log(typeof(costo_presupuesto))
+console.log(typeof(nocambia))
